dc8985d850c4b2e15c1d1b096dbfd2c4d9047453
BT_1N3_19@BKTECH.LOCAL <BT_1N3_19@BKTECH.LOCAL>
2018-11-26 14:46:04 -0500

    Initial commit

---
+++ src/JackOLantern.java
@@ -0,0 +1,19 @@
+public class JackOLantern {
+
+    private String[][] faceFeatures ={{}};
+    private int row = 0;
+    private int column = 0;
+    private String replace;
+
+
+
+    public JackOLantern (String replace, int row, int column) {
+        this.row = row;
+        this.column = column;
+        this.replace = replace;
+    }
+
+    public void edit(String replace, int row, int column) {
+        
+    }
+}
---
+++ src/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        String[][] face = new String[5][8];
+        JackOLantern face1 = new JackOLantern(face);
+
+
+    }
+}
