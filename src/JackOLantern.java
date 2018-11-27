9b6e3cd6c6449962a799a19f10807afa791b98c2
BT_1N3_19 <jlin3392@bths.edu>
2018-11-27 14:40:14 -0500

    Completed

--- src/JackOLantern.java
+++ src/JackOLantern.java
@@ -1,19 +1,36 @@
 public class JackOLantern {
 
     private String[][] faceFeatures ={{}};
-    private int row = 0;
-    private int column = 0;
-    private String replace;
 
 
-
-    public JackOLantern (String replace, int row, int column) {
-        this.row = row;
-        this.column = column;
-        this.replace = replace;
+    public JackOLantern (String[][] faceFeatures) {
+        this.faceFeatures = faceFeatures;
     }
 
     public void edit(String replace, int row, int column) {
-        
+        faceFeatures[row][column] = replace;
     }
+
+    public void fill(String str) {
+        for (int i = 0; i < faceFeatures.length; i++) {
+            for (int j = 0; j < faceFeatures[i].length; j++) {
+                faceFeatures[i][j] = str;
+            }
+        }
+    }
+
+    public String toString() {
+        String line = "";
+
+        for (int i = 0; i < faceFeatures.length; i++) {
+            String row = "";
+            for (int j = 0; j < faceFeatures[i].length; j++) {
+                row += faceFeatures[i][j];
+            }
+            line += row + "\n";
+        }
+
+        return line;
+    }
+
 }
--- src/Runner.java
+++ src/Runner.java
@@ -4,6 +4,24 @@ public class Runner {
         String[][] face = new String[5][8];
         JackOLantern face1 = new JackOLantern(face);
 
+        face1.fill("W");
+        face1.edit(" ", 0, 0);
+        face1.edit(" ", 4, 7);
+        face1.edit(" ", 0, 7);
+        face1.edit(" ", 4, 0);
+
+        face1.edit("O", 1, 2);
+        face1.edit("O", 1, 5);
+        face1.edit("+", 2, 3);
+        face1.edit("+", 2, 4);
+        face1.edit("|", 3, 1);
+        face1.edit("|", 3, 2);
+        face1.edit("|", 3, 3);
+        face1.edit("|", 3, 4);
+        face1.edit("|", 3, 5);
+        face1.edit("|", 3, 6);
+        System.out.println(face1);
+
 
     }
 }
